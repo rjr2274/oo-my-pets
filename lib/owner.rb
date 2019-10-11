f6fdc2ead0c82fb30345bb9a9c74be7ce3bf8a44
Ronald <rjr2274@me.com>
2019-10-11 09:15:37 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,74 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name)
+    @name= name
+    @species= "human"
+    @@all << self
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    @owners_cats = []
+    Cat.all.map {|cat| 
+    if cat.owner == self
+      @owners_cats << cat
+    end}
+    @owners_cats
+  end
+
+  def dogs
+    @owners_dogs = []
+    Dog.all.map {|dog| 
+    if dog.owner == self
+      @owners_dogs << dog
+    end}
+    @owners_dogs
+  end
+
+  def buy_cat(name)
+    Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+
+  def walk_dogs
+    self.dogs.map {|dog| dog.mood = "happy"}
+  end
+
+  def feed_cats
+    self.cats.map {|cat| cat.mood = "happy"}
+  end
+
+  def sell_pets
+    self.dogs.map {|dog| dog.mood = "nervous"
+    dog.owner= nil}
+    self.cats.map {|cat| cat.mood = "nervous"
+    cat.owner= nil}
+  end
+
+  def list_pets
+    "I have #{self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+  end
 end
